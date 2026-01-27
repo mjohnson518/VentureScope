7c130f767e185732ee8abcce1a1ba45c296a6235
mjohnson518 <mjohnson518@users.noreply.github.com>
2026-01-27 11:18:42 -0500

    Add Playwright setup

---
+++ playwright.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig, devices } from '@playwright/test'
+
+export default defineConfig({
+  testDir: './e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [['html', { outputFolder: 'e2e/playwright-report', open: 'never' }]],
+  use: {
+    baseURL: 'https://venturescope.vercel.app',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'Desktop Chrome',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'Desktop Firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Desktop Safari',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'iPad Pro 11',
+      use: { ...devices['iPad Pro 11'] },
+    },
+    {
+      name: 'iPhone 14',
+      use: { ...devices['iPhone 14'] },
+    },
+  ],
+})
